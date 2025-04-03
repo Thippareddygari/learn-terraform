36bc630a8e202854f41b98797354b9b42e6aeff0
kishor <kishorebsc12@gmail.com>
2025-04-03 14:30:25 +0530

    added

---
+++ 07-vault/main.tf
@@ -0,0 +1,20 @@
+provider "vault" {
+  address = "http://vault-internal.kommanuthala.store:8200"
+  token = var.vault_token
+}
+
+variable "vault_token" {}
+
+
+data "vault_kv_secret" "secret_data" {
+  path = "test/data/demo"
+}
+
+# resource "local_file" "test" {
+#   filename = "/tmp/1"
+
+# }
+
+output "name" {
+  value = data.vault_kv_secret.secret_data
+}
